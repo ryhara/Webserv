50db10d372de53b1c19eaccee43822f3b59622f9
ryhara <ryosei.hara@gmail.com>
2023-12-17 15:36:43 +0900

    [Fix] error.cpp

--- srcs/utils/error.cpp
+++ srcs/utils/error.cpp
@@ -1,8 +1,9 @@
 #include "utils.hpp"
+#include "Color.hpp"
 
 void log_exit(const char *msg, int line, const char *file)
 {
-	std::cerr << "Error: " << msg << std::endl;
-	std::cerr << "line : " << line << ", file : " << file << std::endl;
+	std::cerr << RED << "Error: " << msg << END << std::endl;
+	std::cerr << "line : " << UNDERLINE << line << END << ", file : " << UNDERLINE << file << END <<  std::endl;
 	std::exit(EXIT_FAILURE);
 }
