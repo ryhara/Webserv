8aa56bb2249a87015376ece2eafdf26e2e232c08
ryhara <ryosei.hara@gmail.com>
2023-12-19 22:35:31 +0900

    [Add] test surpressoutput.hpp

--- tests/HTTPRequestParse_test.cpp
+++ tests/HTTPRequestParse_test.cpp
@@ -3,14 +3,13 @@
 #include <string>
 #include <sstream>
 #include "../srcs/server/HTTPRequestParse.hpp"
+#include "SurpressOutput.hpp"
 
 char example[] = "GET /cgi/index.html HTTP/1.1\r\nHost: localhost:4242\r\nConnection: keep-alive\r\nsec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"\r\n";
 
 TEST(HTTPRequestParseTest, readRequestLineTest) {
 	// テスト時に標準出力を無視させる
-	std::stringstream buffer;
-	std::streambuf* prevcoutbuf = std::cout.rdbuf();
-	std::cout.rdbuf(buffer.rdbuf());
+	SurpressOutput surpressOutput;
 
 	HTTPRequest request;
 	HTTPRequestParse parse(request);
@@ -19,13 +18,10 @@ TEST(HTTPRequestParseTest, readRequestLineTest) {
 	EXPECT_EQ(request.getMethod(), "GET");
 	EXPECT_EQ(request.getUri(), "/");
 	EXPECT_EQ(request.getVersion(), "HTTP/1.1");
-	std::cout.rdbuf(prevcoutbuf);
 }
 
 TEST(HTTPRequestParseTest, readHeadersTest) {
-	std::stringstream buffer;
-	std::streambuf* prevcoutbuf = std::cout.rdbuf();
-	std::cout.rdbuf(buffer.rdbuf());
+	SurpressOutput surpressOutput;
 
 	char example1[] = "Host: localhost:4242\r\nConnection: keep-alive\r\n";
 	HTTPRequest request;
@@ -34,13 +30,10 @@ TEST(HTTPRequestParseTest, readHeadersTest) {
 	parse.readHeaders(ss);
 	EXPECT_EQ(request.getHeaders()["Host"], "localhost:4242");
 	EXPECT_EQ(request.getHeaders()["Connection"], "keep-alive");
-	std::cout.rdbuf(prevcoutbuf);
 }
 
 TEST(HTTPRequestParseTest, parseTest1) {
-	std::stringstream buffer;
-	std::streambuf* prevcoutbuf = std::cout.rdbuf();
-	std::cout.rdbuf(buffer.rdbuf());
+	SurpressOutput surpressOutput;
 
 	char example2[] = "GET / HTTP/1.1\r\nHost: localhost:4242\r\nConnection: keep-alive\r\n";
 	HTTPRequest request;
@@ -50,13 +43,10 @@ TEST(HTTPRequestParseTest, parseTest1) {
 	EXPECT_EQ(request.getUri(), "/");
 	EXPECT_EQ(request.getVersion(), "HTTP/1.1");
 	// TODO : その他の変数のテストも追加
-	std::cout.rdbuf(prevcoutbuf);
 }
 
 TEST(HTTPRequestParseTest, parseTest2) {
-	std::stringstream buffer;
-	std::streambuf* prevcoutbuf = std::cout.rdbuf();
-	std::cout.rdbuf(buffer.rdbuf());
+	SurpressOutput surpressOutput;
 
 	HTTPRequest request;
 	HTTPRequestParse parse(request);
@@ -67,17 +57,13 @@ TEST(HTTPRequestParseTest, parseTest2) {
 	EXPECT_EQ(request.getHeaders()["Host"], "localhost:4242");
 	EXPECT_EQ(request.getHeaders()["Connection"], "keep-alive");
 	EXPECT_EQ(request.getHeaders()["sec-ch-ua"], "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"");
-	std::cout.rdbuf(prevcoutbuf);
 }
 
 TEST(HTTPRequestParseTest, locationTest) {
-	std::stringstream buffer;
-	std::streambuf* prevcoutbuf = std::cout.rdbuf();
-	std::cout.rdbuf(buffer.rdbuf());
-
+	SurpressOutput surpressOutput;
+	
 	HTTPRequest request;
 	HTTPRequestParse parse(request);
 	parse.parse(example);
 	EXPECT_EQ(request.getLocation(), "/cgi/");
-	std::cout.rdbuf(prevcoutbuf);
 }
