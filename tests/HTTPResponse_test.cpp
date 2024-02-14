bc74af50bddcc15214895efc7c957b435fbdfde2
ryhara <ryosei.hara@gmail.com>
2024-02-14 22:42:20 +0900

    test 修正

--- tests/HTTPResponse_test.cpp
+++ tests/HTTPResponse_test.cpp
@@ -39,7 +39,7 @@ TEST(HTTPResponseTEST, handleNormalRequestTest) {
 	std::string statusLine = "HTTP/1.1 200 OK\r\n";
 	std::string body = readFile.getBody();
 	std::string headers = "Connection: keep-alive\r\nContent-Length: " + ft_to_string(body.length()) + "\r\n" + "Server: webserv/1.0\r\n\r\n";
-	std::string answer = statusLine + headers + body;
+	std::string answer = statusLine + headers + body + "\r\n";
 	SurpressOutput surpressOutput;
 	setRequest();
 
