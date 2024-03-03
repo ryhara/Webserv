38f80bd7a0e78f0607278fb21ffff593a56671be
ryhara <ryosei.hara@gmail.com>
2024-03-03 13:07:55 +0900

    [Fix] python test

--- tests/python/1_POST.py
+++ tests/python/1_POST.py
@@ -1,24 +1,31 @@
 import requests
+import time
 
 url = 'http://localhost:4242'
 
-# POST
-data = {'key1': 'value1'}
-response = requests.post(url, data=data)
+data_list = [
+	{'key': '1'},
+	{'key': '12345'}
+]
 
-# request
-print("--------------------------------------------------------------")
-print("URL:", response.request.url)
-print("Method:", response.request.method)
-print("Headers:")
-for key, value in response.request.headers.items():
-	print(key, ":", value)
-print("Body:", response.request.body)
-print("--------------------------------------------------------------")
-# response
-print("Status Code:", response.status_code)
-print("Headers:")
-for key, value in response.headers.items():
-	print(key, ":", value)
-print(response.text)
-print("==============================================================")
+# POST
+for data in data_list:
+	response = requests.post(url, data=data)
+
+	# request
+	print("--------------------------------------------------------------")
+	print("URL:", response.request.url)
+	print("Method:", response.request.method)
+	print("Headers:")
+	for key, value in response.request.headers.items():
+		print(key, ":", value)
+	print("Body:", response.request.body)
+	print("--------------------------------------------------------------")
+	# response
+	print("Status Code:", response.status_code)
+	print("Headers:")
+	for key, value in response.headers.items():
+		print(key, ":", value)
+	print(response.text)
+	print("==============================================================")
+	time.sleep(1)
